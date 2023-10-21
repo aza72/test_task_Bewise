cef831d54bc3d522a8f7b27b22ebd8dd683e6d53
aza72 <mavtodru@mail.ru>
2023-10-21 20:04:32 +0500

    commit #6

--- tesktask_site/questions/models.py
+++ tesktask_site/questions/models.py
@@ -5,7 +5,8 @@ class Questions(models.Model):
     id_questions = models.TextField(blank=True)
     text = models.TextField(blank=True)
     answer = models.TextField(blank=True)
-    time_create = models.DateTimeField()
+    time_create = models.DateTimeField(blank=True)
 
-    # def __str__(self):
-    #     return self.id
+    def __str__(self):
+
+        return self.id_questions
--- tesktask_site/questions/views.py
+++ tesktask_site/questions/views.py
@@ -1,10 +1,13 @@
 #from django.contrib.sites import requests
 from django.shortcuts import render
 from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
 from rest_framework.utils import json
 from rest_framework.views import APIView
 import requests
+
+import json
 
 from questions.models import Questions
 
@@ -12,10 +15,18 @@ from questions.models import Questions
 class QuestionsAPIView(APIView):
    def post(self,request):
       count=request.GET
+      table_count=Questions.objects.count()
       data = get_data_from_api('https://jservice.io/api/random?',count)
-      answer = read_json(data)
-      save_data(answer)
-      #print (answer)
+      read_json(data)
+      f = Questions.objects.last()
+      g=encode(f)
+      #d = Questions.objects.get(pk=f.id)
+      #s=f.pk-1
+      #=Questions.objects.get(pk=s)
+      #print(table_count)
+      print(g)
+      if table_count==0:
+         data=table_none()
       return Response(data)
 
 
@@ -32,12 +43,33 @@ def read_json(data):
          'answer' : d['answer'],
          'time_create': d['created_at']
       }
+      save_data(answer)
    return answer
 def save_data(data):
-
-   Questions.objects.create(
+   obj, created = Questions.objects.get_or_create(
       id_questions=data['id_questions'],
       text=data['text'],
       answer=data['answer'],
       time_create=data['time_create']
    )
+   if not created:
+      data=get_data_from_api('https://jservice.io/api/random?', 1)
+      read_json(data)
+
+def encode(data):
+
+   date = data.time_create.strftime("%Y-%m-%d %H:%M:%S")
+
+   Json= json.dumps({'id_questions':data.id_questions,
+                     'text':data.text,
+                     'answer':data.answer,
+                     'time_create':date})
+   return Json
+
+def table_none():
+   Json = json.dumps({'id_questions': '',
+                      'text': '',
+                      'answer': '',
+                      'time_create': ''})
+
+   return Json
